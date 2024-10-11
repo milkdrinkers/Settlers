e5f6da1632c143ea2f7ac84a7d8bb00d60490dc0
darksaid98 <darksaid9889@gmail.com>
2024-10-11 02:55:27 +0200

    Initial commit

---
+++ api/build.gradle.kts
@@ -0,0 +1,119 @@
+import java.time.Instant
+
+plugins {
+    `java-library`
+    `maven-publish`
+    eclipse
+    idea
+}
+
+group = rootProject.group
+version = rootProject.version
+description = rootProject.description
+
+val mainPackage = "${project.group}.${project.name.lowercase()}"
+applyCustomVersion()
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21)) // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(libs.annotations)
+    annotationProcessor(libs.annotations)
+
+    compileOnly(libs.paper.api)
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.compilerArgs.addAll(arrayListOf("-Xlint:all", "-Xlint:-processing", "-Xdiags:verbose"))
+    }
+
+    javadoc {
+        isFailOnError = false
+        val options = options as StandardJavadocDocletOptions
+        options.encoding = Charsets.UTF_8.name()
+        options.overview = "src/main/javadoc/overview.html"
+        options.windowTitle = "${rootProject.name} Javadoc"
+        options.tags("apiNote:a:API Note:", "implNote:a:Implementation Note:", "implSpec:a:Implementation Requirements:")
+        options.addStringOption("Xdoclint:none", "-quiet")
+        options.use()
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
+
+fun applyCustomVersion() {
+    // Apply custom version arg or append snapshot version
+    val ver = properties["altVer"]?.toString() ?: "${rootProject.version}-SNAPSHOT-${Instant.now().epochSecond}"
+
+    // Strip prefixed "v" from version tag
+    rootProject.version = (if (ver.first().equals('v', true)) ver.substring(1) else ver.uppercase()).uppercase()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "${rootProject.group}"
+            artifactId = "npc-api"
+            version = "${rootProject.version}"
+
+            pom {
+                name.set("NPC-API")
+                description.set(rootProject.description.orEmpty())
+                url.set("https://github.com/milkdrinkers/NPC-API")
+                licenses {
+                    license {
+                        name.set("GPL-v3.0")
+                        url.set("http://www.gnu.org/licenses/gpl-3.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("darksaid98")
+                        name.set("darksaid98")
+                        organization.set("Milkdrinkers")
+                        organizationUrl.set("https://github.com/milkdrinkers")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/milkdrinkers/NPC-API.git")
+                    developerConnection.set("scm:git:ssh://github.com:milkdrinkers/NPC-API.git")
+                    url.set("https://github.com/milkdrinkers/NPC-API")
+                }
+            }
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "releases"
+            url = uri("https://maven.athyrium.eu/releases")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+        maven {
+            name = "snapshots"
+            url = uri("https://maven.athyrium.eu/snapshots")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
---
+++ core/build.gradle.kts
@@ -0,0 +1,129 @@
+import java.time.Instant
+
+plugins {
+    `java-library`
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.run.paper)
+    alias(libs.plugins.plugin.yml)
+    eclipse
+    idea
+}
+
+group = rootProject.group
+version = rootProject.version
+description = rootProject.description
+
+val mainPackage = "${project.group}.${project.name.lowercase()}"
+applyCustomVersion()
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21)) // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://maven.athyrium.eu/releases")
+    maven("https://maven.citizensnpcs.co/repo")
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:26.0.0")
+    annotationProcessor("org.jetbrains:annotations:26.0.0")
+
+    compileOnly(libs.paper.api)
+
+    // API
+    api(project(":api"))
+    implementation(libs.crate.api)
+    implementation(libs.crate.yaml)
+    implementation(libs.colorparser) {
+        exclude("net.kyori")
+    }
+    implementation(libs.command.api)
+
+    // Plugin Dependencies
+    implementation(libs.bstats.bukkit)
+    compileOnly(libs.citizens) {
+        exclude ("*", "*")
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.compilerArgs.addAll(arrayListOf("-Xlint:all", "-Xlint:-processing", "-Xdiags:verbose"))
+    }
+
+    javadoc {
+        isFailOnError = false
+        val options = options as StandardJavadocDocletOptions
+        options.encoding = Charsets.UTF_8.name()
+        options.overview = "src/main/javadoc/overview.html"
+        options.windowTitle = "${rootProject.name} Javadoc"
+        options.tags("apiNote:a:API Note:", "implNote:a:Implementation Note:", "implSpec:a:Implementation Requirements:")
+        options.addStringOption("Xdoclint:none", "-quiet")
+        options.use()
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    shadowJar {
+        archiveBaseName.set(project.name)
+        archiveClassifier.set("")
+
+        // Shadow classes
+        fun reloc(originPkg: String, targetPkg: String) = relocate(originPkg, "${mainPackage}.lib.${targetPkg}")
+
+        reloc("com.github.milkdrinkers.crate", "crate")
+        reloc("com.github.milkdrinkers.colorparser", "colorparser")
+        reloc("dev.jorel.commandapi", "commandapi")
+        reloc("org.bstats", "bstats")
+
+        minimize()
+    }
+
+    runServer {
+        // Configure the Minecraft version for our task.
+        minecraftVersion("1.21")
+
+        // IntelliJ IDEA debugger setup: https://docs.papermc.io/paper/dev/debugging#using-a-remote-debugger
+        jvmArgs("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005", "-DPaper.IgnoreJavaVersion=true", "-Dcom.mojang.eula.agree=true", "-DIReallyKnowWhatIAmDoingISwear", "-Dpaper.playerconnection.keepalive=6000")
+        systemProperty("terminal.jline", false)
+        systemProperty("terminal.ansi", true)
+    }
+}
+
+bukkit { // Options: https://github.com/Minecrell/plugin-yml#bukkit
+    main = "${mainPackage}.${project.name}"
+
+    // Plugin Information
+    name = project.name
+    prefix = project.name
+    version = "${project.version}"
+    description = "${project.description}"
+    authors = listOf("darksaid98")
+    contributors = listOf()
+    apiVersion = "1.21"
+
+    // Misc properties
+    load = net.minecrell.pluginyml.bukkit.BukkitPluginDescription.PluginLoadOrder.POSTWORLD // STARTUP or POSTWORLD
+    depend = listOf("Citizens")
+    softDepend = listOf("Towny")
+}
+
+fun applyCustomVersion() {
+    // Apply custom version arg or append snapshot version
+    val ver = properties["altVer"]?.toString() ?: "${rootProject.version}-SNAPSHOT-${Instant.now().epochSecond}"
+
+    // Strip prefixed "v" from version tag
+    rootProject.version = (if (ver.first().equals('v', true)) ver.substring(1) else ver.uppercase()).uppercase()
+}
