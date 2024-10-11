bfd968dc2ec8acd6dfdafcdf846a48083ec7f19a
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-10-12 00:54:08 +0200

    feat: reloadable and logger

--- core/src/main/java/io/github/milkdrinkers/settlers/Settlers.java
+++ core/src/main/java/io/github/milkdrinkers/settlers/Settlers.java
@@ -3,4 +3,27 @@ package io.github.milkdrinkers.settlers;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Settlers extends JavaPlugin {
+
+    private static Settlers instance;
+
+
+    public static Settlers getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
 }
