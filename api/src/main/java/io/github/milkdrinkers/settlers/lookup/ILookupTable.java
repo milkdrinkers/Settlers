eb94febd5d749d3081ea778f582d4b1a4c46bac5
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2025-05-13 10:46:37 +0200

    feat: added haskey and hasvalue to lookuptables,

--- api/src/main/java/io/github/milkdrinkers/settlers/lookup/ILookupTable.java
+++ api/src/main/java/io/github/milkdrinkers/settlers/lookup/ILookupTable.java
@@ -5,10 +5,10 @@ import org.jetbrains.annotations.ApiStatus;
 /**
  * Represents a lookup table that can be used to lookup values by keys and vice-versa.
  *
- * @param <T> the key type
- * @param <R> the value type
+ * @param <K> the key type
+ * @param <V> the value type
  */
-public interface ILookupTable<T, R> {
+public interface ILookupTable<K, V> {
     /**
      * Adds a key-value pair to the lookup table.
      *
@@ -16,7 +16,7 @@ public interface ILookupTable<T, R> {
      * @param object2 the value
      */
     @ApiStatus.Internal
-    void add(T object1, R object2);
+    void add(K object1, V object2);
 
     /**
      * Removes a key-value pair from the lookup table by key.
@@ -24,7 +24,7 @@ public interface ILookupTable<T, R> {
      * @param object1 the key
      */
     @ApiStatus.Internal
-    void removeByKey(T object1);
+    void removeByKey(K object1);
 
     /**
      * Removes a key-value pair from the lookup table by value.
@@ -32,7 +32,7 @@ public interface ILookupTable<T, R> {
      * @param object2 the value
      */
     @ApiStatus.Internal
-    void removeByValue(R object2);
+    void removeByValue(V object2);
 
     /**
      * Looks up a key by value.
@@ -40,7 +40,7 @@ public interface ILookupTable<T, R> {
      * @param object2 the value
      * @return the key
      */
-    T lookupKey(R object2);
+    K lookupKey(V object2);
 
     /**
      * Looks up a value by key.
@@ -48,11 +48,27 @@ public interface ILookupTable<T, R> {
      * @param object1 the key
      * @return the value
      */
-    R lookupValue(T object1);
+    V lookupValue(K object1);
 
     /**
      * Clears the lookup table.
      */
     @ApiStatus.Internal
     void clear();
+
+    /**
+     * Checks if a given key exists.
+     *
+     * @param object1 the key.
+     * @return true if the key exists.
+     */
+    boolean hasKey(K object1);
+
+    /**
+     * Checks if a given value exists.
+     *
+     * @param object2 the value
+     * @return true if the value exists.
+     */
+    boolean hasValue(V object2);
 }
--- paper/src/main/java/io/github/milkdrinkers/settlers/lookup/LookupTable.java
+++ paper/src/main/java/io/github/milkdrinkers/settlers/lookup/LookupTable.java
@@ -5,9 +5,9 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 
-public class LookupTable<T, R> implements ILookupTable<T, R> {
-    private final HashMap<T, R> keyMap = new HashMap<>();
-    private final HashMap<R, T> valueMap = new HashMap<>();
+public class LookupTable<K, V> implements ILookupTable<K, V> {
+    private final HashMap<K, V> keyMap = new HashMap<>();
+    private final HashMap<V, K> valueMap = new HashMap<>();
 
     @ApiStatus.Internal
     public LookupTable() {
@@ -15,32 +15,32 @@ public class LookupTable<T, R> implements ILookupTable<T, R> {
 
     @ApiStatus.Internal
     @Override
-    public void add(T object1, R object2) {
+    public void add(K object1, V object2) {
         keyMap.put(object1, object2);
         valueMap.put(object2, object1);
     }
 
     @ApiStatus.Internal
     @Override
-    public void removeByKey(T object1) {
+    public void removeByKey(K object1) {
         valueMap.remove(lookupValue(object1));
         keyMap.remove(object1);
     }
 
     @ApiStatus.Internal
     @Override
-    public void removeByValue(R object2) {
+    public void removeByValue(V object2) {
         keyMap.remove(lookupKey(object2));
         valueMap.remove(object2);
     }
 
     @Override
-    public @Nullable T lookupKey(R object2) {
+    public @Nullable K lookupKey(V object2) {
         return valueMap.get(object2);
     }
 
     @Override
-    public @Nullable R lookupValue(T object1) {
+    public @Nullable V lookupValue(K object1) {
         return keyMap.get(object1);
     }
 
@@ -49,5 +49,13 @@ public class LookupTable<T, R> implements ILookupTable<T, R> {
     public void clear() {
         keyMap.clear();
         valueMap.clear();
+    }
+
+    public boolean hasKey(K object1) {
+        return keyMap.containsKey(object1);
+    }
+
+    public boolean hasValue(V object2) {
+        return valueMap.containsKey(object2);
     }
 }
